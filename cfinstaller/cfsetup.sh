0a400ccb1325c8328df4482dd4fc1830154a2125
Phil Ewels <phil.ewels@scilifelab.se>
2015-04-22 16:38:22 +0200

    Bugfixing the cfinstaller for UPPMAX dev

--- cfinstaller/cfsetup.sh
+++ cfinstaller/cfsetup.sh
@@ -3,13 +3,14 @@ then
     echo "Usage: cfsetup.sh [target installation directory]"
     exit 1
 fi
+STARTDIR=$(pwd)
 mkdir -p $1
 cd $1
 git clone https://github.com/ewels/clusterflow.git .
 git checkout v0.4devel
 git clone https://github.com/ewels/clusterflow-uppmax.git
 mv clusterflow-uppmax/ scripts
-cp ../clusterflow.config.uppmax ./clusterflow.config
+cp ${STARTDIR}/clusterflow.config.uppmax ./clusterflow.config
 echo -e "\n\n## Added by Cluster Flow installer\nexport PATH="'$PATH'":$(pwd)\nexport PATH="'$PATH'":$(pwd)/scripts" >> ~/.bashrc
 ./scripts/cf-uppmax --add_genome
 ./cf --setup
